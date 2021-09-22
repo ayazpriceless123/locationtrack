768e5876ab41c1842c81d72bda875d950f452041
ayaz <ayazpriceless123.com>
2021-09-22 16:24:35 +0500

    commit

---
+++ screens/ExploreScreen.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const ExploreScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Explore Camra Screen</Text>
+ 
+      </View>
+    );
+};
+
+export default ExploreScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+});
